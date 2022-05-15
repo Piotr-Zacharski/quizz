e1fdb3c9638dbf0a95336fc167c17df79a85a71f
Piotr-Zacharski <zacharski.p.h@gmail.com>
2022-05-15 15:58:33 +0200

    howler

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import MyButton from './MyButton'
-import Player from './Player'
-import sound from '../../assets/sound.mp3'
+// import Player from './Player'
+// import sound from '../../assets/sound.mp3'
+import { Howl } from 'howler';
 
-const Intro = (toggle) => {
+const Intro = () => {
+  const sound = new Howl({
+    src: ['../../assets/sound.mp3'],
+    html5: true,
+    preload: true,
+  });
   return (
     <>
     <h1 className="intro-title">FAMILIADA</h1>
       <Link to="/board" className="link">
-        <MyButton title="Nowa gra" onClick={() => toggle}/>
+        <MyButton title="Nowa gra" onClick={() => sound.play()}/>
       </Link>
-      <Player src={sound}/>
+      {/* <Player src={sound}/> */}
     </>
   )
 }
